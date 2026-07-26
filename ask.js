f47391e8192910ddc93a2e07d3d8ed8964bbb9b1
Marcin Er <mar.rudzki@gmail.com>
2026-07-26 07:17:44 +0200

    collect answers in a loop

--- ask.js
+++ ask.js
@@ -3,8 +3,27 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const questions = [
+  'What is your name? ',
+  'Where do you live? ',
+  'What are you going to do with Node.js? ',
+];
 
-rl.question('How do you like Node? ', (answer) => {
-  console.log(`Your answer: ${answer}`);
+function collectAnswers(questions, done) {
+  const answers = [];
+  const questionAnswered = (answer) => {
+    answers.push(answer.trim());
+    if (answers.length < questions.length) {
+      rl.question(questions[answers.length], questionAnswered);
+    } else {
+      return done(answers);
+    }
+  };
+  rl.question(questions[0], questionAnswered);
+}
+
+collectAnswers(questions, (answers) => {
+  console.log('Thank you for your answers!');
+  console.log(answers);
   process.exit();
 });
