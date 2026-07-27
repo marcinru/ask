071276c5b9ef230d4391a073e88e8a143dd671a3
Marcin Er <mar.rudzki@gmail.com>
2026-07-27 05:38:59 +0200

    extract questions to data module

--- ask.js
+++ ask.js
@@ -1,13 +1,11 @@
+const data = require('./questions');
+
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const questions = [
-  'What is your name? ',
-  'Where do you live? ',
-  'What are you going to do with Node.js? ',
-];
 
 function collectAnswers(questions, done) {
   const answers = [];
@@ -22,7 +20,7 @@ function collectAnswers(questions, done) {
   rl.question(questions[0], questionAnswered);
 }
 
-collectAnswers(questions, (answers) => {
+collectAnswers(data.questions, (answers) => {
   console.log('Thank you for your answers!');
   console.log(answers);
   process.exit();
---
+++ questions.js
@@ -0,0 +1,7 @@
+const questions = [
+  'What is your name? ',
+  'Where do you live? ',
+  'What are you going to do with Node.js? ',
+];
+
+module.exports = { questions };
