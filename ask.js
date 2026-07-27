959c4c6dcecd072f5d00d20b45806314ef6ceb5c
Marcin Er <mar.rudzki@gmail.com>
2026-07-27 06:04:26 +0200

    introduce EventEmitter

--- ask.js
+++ ask.js
@@ -1,8 +1,12 @@
 const questions = require('./data/questions');
 const collectAnswers = require('./lib/collectAnswers');
 
-collectAnswers(questions, (answers) => {
+const answerEvents = collectAnswers(questions, (answers) => {
   console.log('Thank you for your answers!');
   console.log(answers);
   process.exit();
+});
+
+answerEvents.on('answer', (answer) => {
+  console.log(`The answer is ${answer}`);
 });
